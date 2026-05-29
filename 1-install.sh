50551f2229d0e5fe5b143de2ade4f80a4984feb4
Kori Tk <66495933+hyprtk@users.noreply.github.com>
2026-05-29 12:17:21 +0100

    Update 1-install.sh

--- 1-install.sh
+++ 1-install.sh
@@ -193,6 +193,7 @@ uninstall_sddm() {
         echo "Removing existing SDDM installation..."
         sudo -A systemctl disable sddm 2>/dev/null || true
         sudo -A pacman -Rns --noconfirm sddm
+        yay -Rns --noconfirm sddm
         echo "SDDM removed."
     else
         echo "SDDM not installed."
