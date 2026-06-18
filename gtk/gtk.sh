15e42eacc2d41b5fd42920c3ae7d6fd776ccf150
Kori Tk <66495933+hyprtk@users.noreply.github.com>
2026-06-18 11:54:35 +0100

    gtk update

--- gtk/gtk.sh
+++ gtk/gtk.sh
@@ -2,5 +2,5 @@
 gnome_schema="org.gnome.desktop.interface"
 gsettings set "$gnome_schema" icon-theme "Papirus-Dark"
 gsettings set "$gnome_schema" cursor-theme "Adwaita"
-gsettings set "$gnome_schema" font-name "Cantarell 10"
+gsettings set "$gnome_schema" font-name "Noto Sans 10"
 gsettings set "$gnome_schema" color-scheme "prefer-dark"
